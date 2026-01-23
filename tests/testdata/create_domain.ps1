86638a33d97ed04585e58bfeb4d2a7025074ae7c
Dave Daly <27152817+codasecdd@users.noreply.github.com>
2026-01-23 14:25:03 +0000

    Add test data

---
+++ tests/testdata/create_domain.ps1
@@ -0,0 +1,14 @@
+Import-Module ADDSDeployment
+
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "testlab.local" `
+-DomainNetbiosName "TESTLAB" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
---
+++ tests/testdata/export_ntds.ps1
@@ -0,0 +1,2 @@
+mkdir c:\users\administrator\desktop\ad
+ntdsutil "activate instance ntds" "ifm" "create full c:\users\administrator\desktop\ad" "q" "q"
